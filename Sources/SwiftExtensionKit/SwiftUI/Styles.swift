0b8cd619ea79ca70dadc9599b6e721b05e1d995b
John Baker <baker2795@gmail.com>
2023-10-16 19:59:43 -0400

    fix glow style

--- Sources/SwiftExtensionKit/SwiftUI/Styles.swift
+++ Sources/SwiftExtensionKit/SwiftUI/Styles.swift
@@ -7,28 +7,30 @@
 
 import SwiftUI
 
-@available(iOS 13, *)
+@available(iOS 17, *)
 public struct PulsatingNeonGlowModifier: ViewModifier {
-    @State private var isPulsating: Bool = false
+    @State private var isPulsating: Bool = false {
+        didSet {
+            DispatchQueue.main.asyncAfter(deadline: .now() + pulseDuration) {
+                withAnimation {
+                    isPulsating.toggle()
+                }
+            }
+        }
+    }
     var pulseDuration: Double = 1.5
     var glowColor: Color = .blue
 
     public func body(content: Content) -> some View {
         content
             .shadow(color: isPulsating ? glowColor : .clear, radius: isPulsating ? 8 : 0)
-            .animation(
-                Animation.easeInOut(duration: pulseDuration)
-                    .repeatForever(autoreverses: true)
-            )
             .onAppear {
-                withAnimation {
-                    self.isPulsating.toggle()
-                }
+                isPulsating.toggle()
             }
     }
 }
 
-@available(iOS 13, *)
+@available(iOS 17, *)
 public extension View {
     func addPulsatingNeonGlow(pulseDuration: Double = 1.5, glowColor: Color = .blue) -> some View {
         self.modifier(PulsatingNeonGlowModifier(pulseDuration: pulseDuration, glowColor: glowColor))
