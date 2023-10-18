4861153e4da2c6bf9671f62447ed0a5971199f62
John Baker <baker2795@gmail.com>
2023-10-17 22:51:12 -0400

    add way to convert emojis to image

--- Sources/SwiftExtensionKit/StringExtensions.swift
+++ Sources/SwiftExtensionKit/StringExtensions.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 public extension String {
     
@@ -34,5 +35,23 @@ public extension String {
         var modifiedString = self
         modifiedString.removeLast()
         return modifiedString
+    }
+    
+    /// Converts text to an image
+    /// Mainly for use with emojis
+    func textToImage() -> UIImage? {
+        let nsString = (self as NSString)
+        let font = UIFont.systemFont(ofSize: 1024) // you can change your font size here
+        let stringAttributes = [NSAttributedString.Key.font: font]
+        let imageSize = nsString.size(withAttributes: stringAttributes)
+
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, 0) //  begin image context
+        UIColor.clear.set() // clear background
+        UIRectFill(CGRect(origin: CGPoint(), size: imageSize)) // set rect size
+        nsString.draw(at: CGPoint.zero, withAttributes: stringAttributes) // draw text within rect
+        let image = UIGraphicsGetImageFromCurrentImageContext() // create image from context
+        UIGraphicsEndImageContext() //  end image context
+
+        return image ?? UIImage()
     }
 }
