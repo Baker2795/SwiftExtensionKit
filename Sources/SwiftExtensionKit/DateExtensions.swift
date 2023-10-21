a1dc268c727a703e90793ef3f59a528be87f0e9e
John Baker <baker2795@gmail.com>
2023-10-21 12:41:22 -0400

    add comments

--- Sources/SwiftExtensionKit/DateExtensions.swift
+++ Sources/SwiftExtensionKit/DateExtensions.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 public extension Date {
+    
+    /// Returns true or false if the date is today
     func isToday() -> Bool {
         let calendar = Calendar.current
         let today = calendar.startOfDay(for: Date())
@@ -16,6 +18,7 @@ public extension Date {
         return calendar.isDate(today, inSameDayAs: selfDate)
     }
     
+    /// Returns difference in minutes of date compared to date passed in
     func minutesDifference(to date: Date) -> Int {
         let calendar = Calendar.current
         let components = calendar.dateComponents([.minute], from: self, to: date)
