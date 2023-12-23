1a916b8a46f49b71ec1f5e80a835a2f551e232b6
John Baker <baker2795@gmail.com>
2023-12-23 15:46:40 -0500

    add

--- Sources/SwiftExtensionKit/SwiftUI/LazyView.swift
+++ Sources/SwiftExtensionKit/SwiftUI/LazyView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 public struct LazyView<Content: View>: View {
-    let build: () -> Content
-    init(_ build: @autoclosure @escaping () -> Content) {
+    public let build: () -> Content
+    public init(_ build: @autoclosure @escaping () -> Content) {
         self.build = build
     }
     public var body: Content {
