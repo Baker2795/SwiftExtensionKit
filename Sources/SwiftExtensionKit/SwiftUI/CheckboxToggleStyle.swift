42fb8a4641b7b064c17615b1ae7a710208b86a93
John Baker <baker2795@gmail.com>
2024-01-11 19:30:17 -0500

    add radio style to ioscheckboxtoggle

--- Sources/SwiftExtensionKit/SwiftUI/CheckboxToggleStyle.swift
+++ Sources/SwiftExtensionKit/SwiftUI/CheckboxToggleStyle.swift
@@ -10,16 +10,42 @@ import SwiftUI
 @available(iOS 14.0, *)
 public struct iOSCheckboxToggleStyle: ToggleStyle {
     
-    public init() { }
+    @State var isRadioStyle: Bool
+    
+    public init(isRadioStyle: Bool = false) {
+        self._isRadioStyle = State(initialValue: isRadioStyle)
+    }
     
     public func makeBody(configuration: Configuration) -> some View {
         Button(action: {
             configuration.isOn.toggle()
         }, label: {
             HStack {
-                Image(systemName: configuration.isOn ? "checkmark.square" : "square")
+                if isRadioStyle {
+                    ZStack(alignment: .center) {
+                        Image(systemName: "circle")
+                        if configuration.isOn {
+                            Image(systemName: "circlebadge.fill")
+                        }
+                    }
+                } else {
+                    Image(systemName: configuration.isOn ? "checkmark.square" : "square")
+                }
                 configuration.label
             }
         })
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    
+    @State var tester: Bool = false
+    
+    return VStack {
+        Toggle(isOn: $tester, label: {
+            Text("thing")
+        })
+        .toggleStyle(iOSCheckboxToggleStyle(isRadioStyle: true))
     }
 }
