3500b14224ae037ac778f50e8225b4551a0820a8
John Baker <baker2795@gmail.com>
2024-07-27 22:35:11 -0400

    make datewrapper optional

--- Sources/SwiftExtensionKit/DateStorage.swift
+++ Sources/SwiftExtensionKit/DateStorage.swift
@@ -8,32 +8,36 @@
 import Foundation
 
 @propertyWrapper
-public struct DateStorage {
+struct DateStorage {
     private let key: String
-    private let defaultValue: Date
+    private let defaultValue: Date?
 
-    public init(wrappedValue: Date, _ key: String) {
+    init(wrappedValue: Date?, _ key: String) {
         self.key = key
         self.defaultValue = wrappedValue
     }
 
-    public var wrappedValue: Date {
+    var wrappedValue: Date? {
         get {
             let timeInterval = UserDefaults.standard.double(forKey: key)
             return timeInterval == 0 ? defaultValue : Date(timeInterval: timeInterval)
         }
         set {
-            UserDefaults.standard.set(newValue.timeInterval, forKey: key)
+            if let newValue = newValue {
+                UserDefaults.standard.set(newValue.timeInterval, forKey: key)
+            } else {
+                UserDefaults.standard.removeObject(forKey: key)
+            }
         }
     }
 }
-
 fileprivate extension Date {
     var timeInterval: TimeInterval {
         return self.timeIntervalSince1970
     }
 
-    init(timeInterval: TimeInterval) {
+    init?(timeInterval: TimeInterval?) {
+        guard let timeInterval = timeInterval else { return nil }
         self = Date(timeIntervalSince1970: timeInterval)
     }
 }
